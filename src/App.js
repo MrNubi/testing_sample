6d7a7429eca8a5bf1afe3a4bcdbdb672877b1958
jahun <99630193+MrNubi@users.noreply.github.com>
2022-12-08 20:54:05 +0900

    changes  test commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 data-testId="counter">{count}</h3>
+        <div className="btn-area" style={{ display: 'flex' }}>
+          <button
+            disabled={disabled}
+            style={{ margin: '10px' }}
+            data-testId="minus-button"
+            onClick={() => {
+              setCount((prev) => prev - 1);
+            }}
+          >
+            -
+          </button>
+
+          <button
+            disabled={disabled}
+            style={{ margin: '10px' }}
+            data-testId="plus-button"
+            onClick={() => {
+              setCount((prev) => prev + 1);
+            }}
+          >
+            +
+          </button>
+        </div>
+        <button
+          data-testId="on/off-button"
+          style={{ backgroundColor: 'blue' }}
+          onClick={() => {
+            setDisabled((prev) => !prev);
+          }}
+        >
+          on/off
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
+// render 함수
+/* 
+  DOM에 컴포넌트를 랜더링하는 함수
+  -> 인자로, 랜더링 할 리액트 컴포넌트 들어감 
+*/
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test('minus button has correct text', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('minus-button');
+  expect(buttonElement).toHaveTextContent('-');
+});
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('plus-button');
+  expect(buttonElement).toHaveTextContent('+');
+});
+
+test('when + btn is clicked, counter increase 1', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('plus-button');
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent('1');
+});
+test('when - btn is clicked, counter decrease 1', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('minus-button');
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(-1);
+});
+
+test('on/off btn has blue background', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('on/off-button');
+
+  expect(buttonElement).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+test('when on/off btn clicked, plus-btn and minus-btn has prevent their press or not', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('on/off-button');
+  fireEvent.click(buttonElement);
+  const plusButtonElement = screen.getByTestId('plus-button');
+  const minusButtonElement = screen.getByTestId('minus-button');
+
+  expect(plusButtonElement).toBeDisabled();
+  expect(minusButtonElement).toBeDisabled();
 });
